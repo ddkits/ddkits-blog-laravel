51d29ac11739109a688bb7003cae3904c5ae029e
Sam Elayyoub <m22386@live.com>
2020-03-29 20:03:16 -0700

    Fixing facebook Import

--- app/Http/Controllers/fbConfig.php
+++ app/Http/Controllers/fbConfig.php
@@ -16,10 +16,10 @@ use Facebook\Exceptions\FacebookSDKException;
  * Configuration and setup Facebook SDK
  */
 
-$appId = 'InsertAppID'; //Facebook App ID
-$appSecret = 'InsertAppSecret'; //Facebook App Secret
-$redirectURL = 'http://localhost/post_to_facebook_from_website/'; //Callback URL
-$fbPermissions = array('publish_actions'); //Facebook permission
+// $appId = 'InsertAppID'; //Facebook App ID
+// $appSecret = 'InsertAppSecret'; //Facebook App Secret
+// $redirectURL = 'http://localhost/post_to_facebook_from_website/'; //Callback URL
+// $fbPermissions = array('publish_actions'); //Facebook permission
 
 // $fb = new Facebook(array(
 //     'app_id' => env('FACEBOOK_APP_ID'),
@@ -27,20 +27,20 @@ $fbPermissions = array('publish_actions'); //Facebook permission
 //     'default_graph_version' => 'v2.6',
 // ));
 
-// Get redirect login helper
-$helper = $fb->getRedirectLoginHelper();
+// // Get redirect login helper
+// $helper = $fb->getRedirectLoginHelper();
 
-// Try to get access token
-try {
-    if (isset($_SESSION['facebook_access_token'])) {
-        $accessToken = $_SESSION['facebook_access_token'];
-    } else {
-        $accessToken = $helper->getAccessToken();
-    }
-} catch (FacebookResponseException $e) {
-    echo 'Graph returned an error: '.$e->getMessage();
-    exit;
-} catch (FacebookSDKException $e) {
-    echo 'Facebook SDK returned an error: '.$e->getMessage();
-    exit;
-}
+// // Try to get access token
+// try {
+//     if (isset($_SESSION['facebook_access_token'])) {
+//         $accessToken = $_SESSION['facebook_access_token'];
+//     } else {
+//         $accessToken = $helper->getAccessToken();
+//     }
+// } catch (FacebookResponseException $e) {
+//     echo 'Graph returned an error: '.$e->getMessage();
+//     exit;
+// } catch (FacebookSDKException $e) {
+//     echo 'Facebook SDK returned an error: '.$e->getMessage();
+//     exit;
+// }
