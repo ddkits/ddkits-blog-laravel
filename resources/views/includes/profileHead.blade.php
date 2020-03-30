151b304cb267872083496193068cbbb94dbcfcfb
Sam Elayyoub <m22386@live.com>
2020-03-29 23:03:38 -0700

    Fixing feeds board instead of blogs

--- resources/views/includes/profileHead.blade.php
+++ resources/views/includes/profileHead.blade.php
@@ -9,13 +9,16 @@
         <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
     <!-- Bootstrap core CSS -->
+    <script src="/vendor/popper/popper.min.js"></script>
+<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Custom styles for this template -->
+<script src="//code.jquery.com/jquery-1.12.4.js"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <!-- Custom styles for this template -->
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Styles -->
     <link href="/css/profile.css" rel="stylesheet">
-    <script src="//code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <link rel="stylesheet" href="/css/bootstrap2.min.css" rel="stylesheet" id="theme-stylesheet">
     {{--  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">  --}}
 
